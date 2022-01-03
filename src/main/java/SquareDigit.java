8fe164fc8da39213d8c0b08833ce9118b37f56d2
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2022-01-03 12:18:33 +0200

    Using while loop, adding .gitignore

--- src/main/java/SquareDigit.java
+++ src/main/java/SquareDigit.java
@@ -1,12 +1,13 @@
 public class SquareDigit {
     public int squareDigits(int n) {
-        int numberOfDigits = String.valueOf(n).length();
         StringBuilder builder = new StringBuilder();
-
-        for (int i = numberOfDigits - 1; i >= 0; i--) {
-            int number = (int) (n / (Math.pow(10, i)));
-            builder.append(number * number);
-            n -= number * (Math.pow(10, i));
+        if(n==0){
+            return 0;
+        }
+        while (n>0) {
+            int number = n%10;
+            builder.insert(0,number * number);
+            n /= 10;
         }
         return Integer.parseInt(builder.toString());
     }

710bab40f242bca46c3543fd28d62ce15fb7e112
DmitryLazyuka <dmitrylazyuka@gmail.com>
2022-01-05 14:41:50 +0200

    Some changes

--- src/main/java/Accumul.java
+++ src/main/java/Accumul.java
@@ -3,11 +3,11 @@ public class Accumul {
     public static String accum(String s) {
 
         StringBuilder builder = new StringBuilder();
-        String[] characters = s.split("(?!^)");
+        String[] characters = s.split("");
 
         for (int i = 0; i < characters.length; i++) {
             builder.append(characters[i].toUpperCase()).append(characters[i].toLowerCase().repeat(i));
-            if (i != (characters.length - 1)) {
+            if (i != characters.length - 1) {
                 builder.append("-");
             }
         }

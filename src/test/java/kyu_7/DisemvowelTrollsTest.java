43ab9c1d29364bb5b9d5db31b65f4a230e94804f
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2022-01-08 12:40:19 +0200

    https://www.codewars.com/kata/52fba66badcd10859f00097e/train/java

---
+++ src/test/java/kyu_7/DisemvowelTrollsTest.java
@@ -0,0 +1,16 @@
+package kyu_7;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class DisemvowelTrollsTest {
+    @Test
+    public void FixedTests() {
+        assertEquals("Ths wbst s fr lsrs LL!",DisemvowelTrolls.disemvowel("This website is for losers LOL!")
+        );
+        assertEquals("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd", DisemvowelTrolls.disemvowel(
+                "No offense but,\nYour writing is among the worst I've ever read"));
+        assertEquals( "Wht r y,  cmmnst?", DisemvowelTrolls.disemvowel("What are you, a communist?"));
+    }
+}

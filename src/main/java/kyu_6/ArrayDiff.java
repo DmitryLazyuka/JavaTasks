8f9aefbb5b65dd172463526cac5068b572b92603
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2022-01-09 14:23:26 +0200

    https://www.codewars.com/kata/523f5d21c841566fde000009/train/java

---
+++ src/main/java/kyu_6/ArrayDiff.java
@@ -0,0 +1,17 @@
+package kyu_6;
+
+import java.util.*;
+
+public class ArrayDiff {
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+
+        List<Integer> listA = new ArrayList<>();
+        for (int x : a) listA.add(x);
+        List<Integer> listB = new ArrayList<>();
+        for (int x : b) listB.add(x);
+        listA.removeAll(listB);
+
+        return listA.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

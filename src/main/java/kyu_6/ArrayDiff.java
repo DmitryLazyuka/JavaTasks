078c81cae92620be2e7a69477dbd7a6b8a3de447
DmitryLazyuka <dmitrylazyuka@gmail.com>
2022-01-10 10:17:23 +0200

    code style changes

--- src/main/java/kyu_6/ArrayDiff.java
+++ src/main/java/kyu_6/ArrayDiff.java
@@ -1,15 +1,21 @@
 package kyu_6;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayDiff {
 
     public static int[] arrayDiff(int[] a, int[] b) {
 
         List<Integer> listA = new ArrayList<>();
-        for (int x : a) listA.add(x);
+        for (int x : a) {
+            listA.add(x);
+        }
         List<Integer> listB = new ArrayList<>();
-        for (int x : b) listB.add(x);
+        for (int x : b) {
+            listB.add(x);
+        }
+
         listA.removeAll(listB);
 
         return listA.stream().mapToInt(Integer::intValue).toArray();

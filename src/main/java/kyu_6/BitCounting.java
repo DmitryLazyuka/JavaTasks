e287d9e0e0e36d89ab5f7e6b501f673a609e33e2
DmitryLazyuka <dmitrylazyuka@gmail.com>
2022-01-12 12:12:18 +0200

    Another implementation

--- src/main/java/kyu_6/BitCounting.java
+++ src/main/java/kyu_6/BitCounting.java
@@ -1,26 +1,26 @@
 package kyu_6;
 
 public class BitCounting {
-    public static int countBits(int n){
+    public static int countBits(int n) {
 
         String[] binary = Integer.toBinaryString(n).split("");
-        int count =0;
+        int count = 0;
 
-        for(String number : binary){
-            if (number.equals("1")){
+        for (String number : binary) {
+            if (number.equals("1")) {
                 count++;
             }
         }
         return count;
     }
 
-    public static int countBits2(int n){
-        int count=0;
-        while (n>0){
-            if(n%2!=0){
+    public static int countBits2(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 != 0) {
                 count++;
             }
-            n/=2;
+            n /= 2;
         }
         return count;
     }

48234aa450d74a9b6b35507e123595d5fab632ad
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2022-01-22 12:50:58 +0200

    Best Time To Buy

--- src/main/java/leetcode/BestTimeToBuy.java
+++ src/main/java/leetcode/BestTimeToBuy.java
@@ -5,9 +5,9 @@ public class BestTimeToBuy {
 
         int profit = 0;
 
-        for (int i = 0; i < prices.length-1; i++){
-            if (prices[i]<prices[i+1]){
-                profit += prices[i+1]-prices[i];
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                profit += prices[i + 1] - prices[i];
             }
         }
         return profit;

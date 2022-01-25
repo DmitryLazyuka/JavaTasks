12137ce61a9f8c21c8bf9c4446dc0d4667454168
DmitryLazyuka <dmitrylazyuka@gmail.com>
2022-01-25 12:23:26 +0200

    Leetcode 120.Triangle

--- src/main/java/leetcode/Triangle.java
+++ src/main/java/leetcode/Triangle.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 public class Triangle {
     public static int minimumTotal(List<List<Integer>> triangle) {
-
+        int temp;
         for (int i = triangle.size() - 1; i > 0; i--) {
-            int temp;
-
             for (int j = 0; j <= triangle.get(i).size() - 2; j++) {
                 temp = triangle.get(i - 1).get(j);
                 temp += Math.min(triangle.get(i).get(j), triangle.get(i).get(j + 1));
--- src/test/java/leetcode/TriangleTest.java
+++ src/test/java/leetcode/TriangleTest.java
@@ -2,7 +2,6 @@ package leetcode;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 

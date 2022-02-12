35ca4fea97a45c1446e3499f92f6ef30353082a2
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2022-02-12 22:25:16 +0200

    Code simplify

--- src/main/java/kyu_6/TwoSum.java
+++ src/main/java/kyu_6/TwoSum.java
@@ -6,10 +6,10 @@ public class TwoSum {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target) {
-                    return new int[] {i, j};
+                    return new int[]{i, j};
                 }
             }
         }
-        return null;
+        return new int[0];
     }
 }

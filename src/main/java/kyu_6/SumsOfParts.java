fd9ceaddfeeb977426748b9cee7c0daf8e6a1b26
DmitryLazyuka <dmitrylazyuka@gmail.com>
2022-02-22 16:53:12 +0200

    Sum of parts

--- src/main/java/kyu_6/SumsOfParts.java
+++ src/main/java/kyu_6/SumsOfParts.java
@@ -1,17 +1,15 @@
 package kyu_6;
 
 //https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/java
-import java.util.stream.IntStream;
 
 public class SumsOfParts {
     public static int[] sumParts(int[] ls) {
-        int sum = IntStream.of(ls).sum();
-        int[]result = new int[ls.length+1];
-        result[0] = sum;
+        int[] result = new int[ls.length + 1];
+        int sum = 0;
 
-        for (int i = 0; i< ls.length; i++){
-            sum -= ls[i];
-            result[i+1] = sum;
+        for (int i = ls.length-1; i >= 0; i--) {
+            sum += ls[i];
+            result[i] = sum;
         }
         return result;
     }
